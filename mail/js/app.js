3bbc1ec382e73895f34cf0489d6271ab4785e99e
giannisaladino <saladinogianni6@gmail.com>
2024-03-07 16:50:38 +0100

    ADD: dom element

--- mail/js/app.js
+++ mail/js/app.js
@@ -1,12 +1,17 @@
 // 1 RICHIAMO GLI ELEMENTI DI INPUT DALL'HTML
 
+let containerElement = document.querySelector('.container');
+
 const inputEmailDOMElement = document.getElementById('email');
 const buttonDOMElement = document.querySelector('.submit');
 
 // 2 CONTROLLO CHE LA MAIL INVIATA DALL'UTENTE SIA PRESENTE NELLA LISTA DI CHI PUO ACCEDERE
 
 //  - dichiaro un array con i soli utenti che hanno l'accesso
-const autorizedUsers = ['pippo@gmail.com', 'ciao@libero.it'];
+const autorizedUsers = ['pippo@gmail.com', 'ciao@libero.it', 'pippo@pluto.com'];
+
+
+let trovato = false;
 
 //  - nel momento in cui l'utente invierà la sua mail tramite il 'submit', verifico se è presente nella lista con un ciclo
 
@@ -19,16 +24,22 @@ buttonDOMElement.addEventListener('click', function () {
         
         // - SE è presente stampo il messaggio 'accesso consentito'
         if (userEmail === autorizedUsers[i]) {
-            console.log('accesso consentito');
+            trovato = true;
         }
-        // - ALTRIMENTI stampo il messaggio 'accesso negato'
-        // else {
-        //     console.log('accesso negato');
-        // } 
     }
+
+    if(trovato === true) {
+        containerElement.innerHTML += `<h2 class="green">Benvenuto, sei in lista!</h2>`;
+        // console.log('accesso consentito');
+        // console.log('accesso consentito');
     
-    
-    
+    } else {
+        containerElement.innerHTML += `<h2 class="red">Oh no, non sei presente in lista :(</h2>`;
+        // console.log('accesso negato');
+    }
 })
+
+
+
 
 
