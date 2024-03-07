653d1199117d62aa4e5942bc538767d3ec8c13e7
giannisaladino <saladinogianni6@gmail.com>
2024-03-07 16:06:28 +0100

    edit for

--- mail/js/app.js
+++ mail/js/app.js
@@ -8,18 +8,27 @@ const buttonDOMElement = document.querySelector('.submit');
 //  - dichiaro un array con i soli utenti che hanno l'accesso
 const autorizedUsers = ['pippo@gmail.com', 'ciao@libero.it'];
 
-//  - nel momento in cui l'utente invierà la sua mail tramite il 'submit', verifico se è presente nella lista
+//  - nel momento in cui l'utente invierà la sua mail tramite il 'submit', verifico se è presente nella lista con un ciclo
 
-buttonDOMElement.addEventListener('click', function(){
+buttonDOMElement.addEventListener('click', function () {
 
-    userEmail = inputEmailDOMElement.value;
+    const userEmail = inputEmailDOMElement.value;
 
-    // - SE è presente stampo il messaggio 'accesso consentito'
-    if(userEmail === autorizedUsers) {
-        console.log('accesso consentito');
+    for(let i = 0; i < autorizedUsers.length; i++) {
+        // console.log(autorizedUsers[i]);
+        
+        // - SE è presente stampo il messaggio 'accesso consentito'
+        if (userEmail === autorizedUsers[i]) {
+            console.log('accesso consentito');
+        }
+        // - ALTRIMENTI stampo il messaggio 'accesso negato'
+        // else {
+        //     console.log('accesso negato');
+        // } 
     }
-    // - ALTRIMENTI stampo il messaggio 'accesso negato'
-     else {
-        console.log('accesso negato');
-     } 
+    
+    
+    
 })
+
+
